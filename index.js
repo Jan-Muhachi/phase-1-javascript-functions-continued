363c2fce754263c2d5542bab19566240d616361a
Jan Muhachi <jan.muhachi@gmail.com>
2024-03-27 12:18:50 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+
+
+//creat a function called saturday fun
+function saturdayFun(activity="roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+  }
+ // write another function that will tell us what we have to do on saturday.
+ 
+  let mondayWork = function(activity="go to the office") {
+    return `This Monday, I will ${activity}.`
+  }
+  // write a wrap adjective function
+  let wrapAdjective = function(style="*") {
+    return function(adjective="special") {
+      return `You are ${ style }${ adjective }${ style } !`
+    }
+  }
